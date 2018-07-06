e0751bb5cb95c140f6aedce992abaae1899b0ded
Alex Lancaster <alancast@umich.edu>
2018-07-06 14:19:22 -0700

    Add styling and text input I worked further through the tutorial. Got through text input

--- HelloWorldProject/App.js
+++ HelloWorldProject/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-        <Bananas/>
-        <Greeting name='Jessalyn' />
-        <Greeting name='Alex' />
-        <Greeting name='Mike' />
-        <Blink text='I love to blink' />
+      <View style={{flex: 1}}>
+        <View style={styles.yellowContainer}>
+          <Text style={{marginTop: 50}}>Hello world!</Text>
+          <Bananas/>
+          <Greeting name='Jessalyn' />
+          <Greeting name='Alex' />
+          <Greeting name='Mike' />
+          <Blink text='I love to blink' />
+          <PizzaTranslator/>
+        </View>
+        <View style={styles.blueContainer}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>Hello world!</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  blueContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 50
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  yellowContainer: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   }
 });
 
@@ -60,6 +73,29 @@ class Greeting extends Component {
   render() {
     return (
       <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+          backgroundColor='white'
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
     );
   }
 }
